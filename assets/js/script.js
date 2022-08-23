8c0eaf180d6d769e5b97e568c9d36f55f410f71d
Monahlissa <salissa4@gmail.com>
2022-08-23 13:35:12 -0500

    updated css, index, add js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,76 @@
-//need help
+//Questions for quiz
+var questions = [
+    {
+        questionText:
+            "Where did Yves Saint Laurent begin his career?",
+        choices: ["Lanvin", "Hermes", "Dior", "Balmain"],
+        answer: "Dior"
+    },
+    {
+        questionText:
+            "What year was Gucci founded in?",
+        choices: ["1917", "1931", "1923", "1921"],
+        answer: "1921"
+    },
+    {
+        questionText:
+            "From 1983 until 2019, Karl Lagerfield was the creative director for which brand?",
+        choices: ["Alaia", "Chanel", "Oscar de la Renta", "Pucci"],
+        answer: "Chanel"
+    },
+    {
+        questionText:
+            "Who is the current creative director for Balmain?",
+        choices: ["Oliver Rousteing", "Riccardo Tisci", "Alessandro Michele", "Maria Chiuri"],
+        answer: "Oliver Rouseteing"
+    },
+    {
+        questionText:
+            "What country was Alexander McQueen from?",
+        choices: ["France", "Italy", "United States", "London"],
+        answer: "London"
+    },
+]
+var index = 0
+var questionsArea = document.querySelector("#questions")
+var timerEl = document.querySelector('.timer');
+var starQuiz = document.querySelector("#begin")
+
+//begin quiz timer 
+function playgame() {
+    var timeLeft = 60;
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            console.log("hi")
+            timeLeft--;
+            timerEl.innerText = 'Time: ' + timeLeft;
+
+        } else {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+};
+
+//Displays questions
+starQuiz.addEventListener("click", function () {
+    playgame()
+    starQuiz.classList.add("hide")
+    displayQuestion()
+});
+
+function displayQuestion() {
+    questionsArea.innerHTML = ""
+    var text = document.createElement("h2")
+    text.innerText = questions[index].questionText
+    questionsArea.appendChild(text)
+    for (var i = 0; i < questions[index].choices.length; i++) {
+        var button = document.createElement("button")
+        button.innerText = questions[index].choices[i]
+        button.addEventListener("click" , checkAnswer)
+        questionsArea.appendChild(button)
+    }
+};
+
+function checkAnswer() {
+    
+}
